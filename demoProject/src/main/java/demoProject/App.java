9cb0454fb94fd9b66eb6f1ce135ed97960a98644
Paul Winstone <paul.winstone@intertek.com>
2020-06-11 16:20:13 +0100

    Project commit

---
+++ demoProject/src/main/java/demoProject/App.java
@@ -0,0 +1,15 @@
+package demoProject;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hi - demo Module here");
+
+	}
+
+	public boolean someMethod() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
